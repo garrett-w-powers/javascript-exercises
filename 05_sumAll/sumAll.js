9675ee5826ffb7a7bd32b72e43a433e31417c001
garrett_powers <garrett.powers@agnet.tamu.edu>
2024-02-12 17:29:20 -0600

    Sum all and leap years done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((typeof num1 !== 'number' | typeof num2 !== 'number')){
+        return 'ERROR';
+    }
+    else if ((num1 < 0 | num2 < 0)){
+        return 'ERROR';
+    }
+    const start = Math.min(num1, num2);
+    const end = Math.max(num1, num2);
+    const length = end - start + 1;
+    let toAdd = Array(length).fill().map((element, index) => index + start)
+    console.log(toAdd)
+    const sum = toAdd.reduce((partialSum, a) => partialSum + a, 0);
+    return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const check1 = (yr) => {
+        if (yr % 4 == 0) return true
+        else return false;
+    };
+    const check2 = (yr) => {
+        if((yr % 100 == 0 && yr % 400 == 0)) {
+            return true;
+        }
+        else return false;
+    };
+    console.log(check1(year));
+    console.log(check2(year));
 
+    if (check1(year)) {
+        if (year % 100 == 0) {
+            return check2(year)
+        }
+        else return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
