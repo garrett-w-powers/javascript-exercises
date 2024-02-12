e923269069b14f9ed50b60402a32f84197ea00ab
GARRETT POWERS <GARRETT.POWERS@AGNET.TAMU.EDU>
2024-02-11 20:53:57 -0600

    Next two!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const letters = str.split('');
+    const reversed = letters.reverse();
+    return reversed.join('');
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    let output = arguments[0]
+    for (let i = 1; i <= arguments.length; i++){
+        const index = output.indexOf(arguments[i])
+        if (index > -1){
+            output.splice(index, 1)
+        }
+    }
+    return output;
+    
 };
 
 // Do not edit below this line
