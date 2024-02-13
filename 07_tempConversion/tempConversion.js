7a8db599586944962c1b1343f0fc6aeb7c57a98f
garrett_powers <garrett.powers@agnet.tamu.edu>
2024-02-13 16:40:03 -0600

    Finish temps

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    let tempTemp = temp - 32;
+    tempTemp = tempTemp / 1.8;
+    const output = Math.round(tempTemp * 10) / 10;
+    return output;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempTemp = temp * 1.8;
+  tempTemp = tempTemp + 32;
+  const output = Math.round(tempTemp * 10) / 10;
+  return output;
 };
 
 // Do not edit below this line
