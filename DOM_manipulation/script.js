435adf6f4a04d3167ef0de5404b2ee563f4a9344
GARRETT POWERS <GARRETT.POWERS@AGNET.TAMU.EDU>
2024-02-14 09:56:59 -0600

    finish exercise

--- DOM_manipulation/script.js
+++ DOM_manipulation/script.js
@@ -5,3 +5,33 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const imRed = document.createElement('p');
+imRed.classList.add('red');
+imRed.style.color = 'red';
+imRed.textContent = 'Hey I\'m red!'
+
+container.appendChild(imRed);
+
+const blueHeader = document.createElement('h3');
+blueHeader.classList.add('blue');
+blueHeader.textContent = 'I\'m a blue h3';
+
+container.appendChild(blueHeader);
+
+const box = document.createElement('div');
+box.classList.add('box');
+box.setAttribute('style', 'background: pink; border-style: solid; border-color: black;');
+
+const title = document.createElement('h1');
+title.textContent = "I'm h1 in the div!"
+title.classList.add('title');
+
+const divText = document.createElement('p');
+divText.textContent = 'Me too!'
+divText.classList.add('divContent');
+
+box.appendChild(title);
+box.appendChild(divText);
+
+container.appendChild(box);
