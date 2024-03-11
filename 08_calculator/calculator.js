30f9f98fa91414d22492d73d378e1ae7ad2c90be
GARRETT POWERS <GARRETT.POWERS@AGNET.TAMU.EDU>
2024-03-11 13:25:03 -0500

    Finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr = []) {
+	const total = arr.reduce((acc, x) => acc + x, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr = []) {
+  const total = arr.reduce((acc, x) => acc * x, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(len) {
+	return Array.from(Array(len), (e,i)=>i+1).reduce((acc, x) => acc * x, 1);
 };
 
 // Do not edit below this line
