39638acf300c47f0b784fef7640ae04d79ba5dab
GARRETT POWERS <GARRETT.POWERS@AGNET.TAMU.EDU>
2024-03-11 13:44:23 -0500

    Palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const chars = Array.from(input.toLowerCase().replace(/[^0-9a-z]/gi, ''));
+    console.log(chars);
+    const revChars = Array.from(chars).reverse();
+    console.log(revChars);
+    return chars.every((character, index) => {
+        console.log(character);
+        console.log(revChars[index]);
+        return character === revChars[index]});
 };
 
 // Do not edit below this line
